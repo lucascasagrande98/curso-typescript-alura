cae526627140ddf90ea60f14896e8aedb0d1905f
lucascasagrande98 <lucascasagrande98@gmail.com>
2023-02-27 21:44:01 -0300

    parte 2 concluida

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -5,8 +5,16 @@ export class Negociacao {
 		return this.quantidade * this.valor;
 	}
 
-  get data(): Date {
-    const data = new Date(this._data.getTime());
-    return data;
+	get data(): Date {
+		const data = new Date(this._data.getTime());
+		return data;
+	}
+
+  public static criaDe(dataString: string, quantidadeString: string, valorString: string): Negociacao {
+    const exp = /-/g;
+		const date = new Date(dataString.replace(exp, ","));
+		const quantidade = parseInt(quantidadeString);
+		const valor = parseFloat(valorString);
+		return new Negociacao(date, quantidade, valor);
   }
 }
