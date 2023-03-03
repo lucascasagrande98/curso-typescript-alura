27001440b56ae45c758f70d036dec9ca44d2951c
lucascasagrande98 <lucascasagrande98@gmail.com>
2023-03-03 14:08:27 -0300

    Projeto concluido

---
+++ app/dist/js/models/negociacoes.js
@@ -0,0 +1,15 @@
+export class Negociacoes {
+    constructor() {
+        this.negociacoes = [];
+    }
+    adiciona(negociacao) {
+        this.negociacoes.push(negociacao);
+    }
+    lista() {
+        return this.negociacoes;
+    }
+    paraTexto() {
+        return JSON.stringify(this.negociacoes, null, 2);
+    }
+}
+//# sourceMappingURL=negociacoes.js.map
